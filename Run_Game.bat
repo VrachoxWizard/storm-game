5372f4d91b8ccacb7928730a08275e54e2de8710
Narcox <mvukusic67@gmail.com>
2026-09-05 18:13:44 +0200

    feat: add quick launcher scripts for Godot Editor and Game

---
+++ Run_Game.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" "C:\Users\user1\AppData\Local\Microsoft\WinGet\Packages\GodotEngine.GodotEngine_Microsoft.Winget.Source_8wekyb3d8bbwe\Godot_v4.7.2-stable_win64.exe" --path "%~dp0."
